0c649ad3898b4cd616acb417f05888b0449e3659
hasin-shabbir <mhs581@nyu.edu>
2021-11-03 01:24:40 -0400

    setup initial react app for front-end

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 //for sessions
@@ -21,58 +23,60 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.redirect('/login');
-});
+// app.get('/', (req, res) => {
+//     res.redirect('/login');
+// });
 
-app.get('/register', (req, res) => {
-    //registration page
-});
+// app.get('/register', (req, res) => {
+//     //registration page
+// });
 
-app.post('/register', (req, res) => {
-    //registration form handling
-    res.redirect('/course-list'); //redirect to course-list upon successful registration by logging in the user
-});
+// app.post('/register', (req, res) => {
+//     //registration form handling
+//     res.redirect('/course-list'); //redirect to course-list upon successful registration by logging in the user
+// });
 
-app.get('/login', (req, res) => {
-    //login page
-});
+// app.get('/login', (req, res) => {
+//     //login page
+// });
 
-app.post('/login',(req,res)=>{
-    //login form handling
-    res.redirect('/course-list'); //redirect to course-list upon successful login
-})
+// app.post('/login',(req,res)=>{
+//     //login form handling
+//     res.redirect('/course-list'); //redirect to course-list upon successful login
+// })
 
-app.get('/course-list',(req,res)=>{
-    //handle any query parameters here
-    //displaying the course list page
-});
+// app.get('/course-list',(req,res)=>{
+//     //handle any query parameters here
+//     //displaying the course list page
+// });
 
-app.get('/review-page/:course',(req,res)=>{
-    //rendering the review page for the particular course in the request parameters
-});
+// app.get('/review-page/:course',(req,res)=>{
+//     //rendering the review page for the particular course in the request parameters
+// });
 
-app.get('/write-a-review/:course',(req,res)=>{
-    //rendering the write a review page for the particular course in the request parameters
-});
+// app.get('/write-a-review/:course',(req,res)=>{
+//     //rendering the write a review page for the particular course in the request parameters
+// });
 
-app.post('/write-a-review/:course',(req,res)=>{
-    //handling form for writing a review for the particular course
-    res.redirect('/write-a-review/:course') //redirect to reviews page for the course upon successful review post
-});
+// app.post('/write-a-review/:course',(req,res)=>{
+//     //handling form for writing a review for the particular course
+//     res.redirect('/write-a-review/:course') //redirect to reviews page for the course upon successful review post
+// });
 
-app.get('/edit-review/:course',(req,res)=>{
-    //rendering the edit a review page for the logged in user for the particular course in the request parameters
-});
+// app.get('/edit-review/:course',(req,res)=>{
+//     //rendering the edit a review page for the logged in user for the particular course in the request parameters
+// });
 
-app.post('/edit-review/:course',(req,res)=>{
-    //handling form for editing a review for the particular course
-    res.redirect('/edit-a-review/:course') //redirect to reviews page for the course upon successful review edit
-});
+// app.post('/edit-review/:course',(req,res)=>{
+//     //handling form for editing a review for the particular course
+//     res.redirect('/edit-a-review/:course') //redirect to reviews page for the course upon successful review edit
+// });
 
 //ENDPOINT FOR API to respond to GET requests from the React App
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: `Hello from server ${PORT}` });
 });
 
-app.listen(3000,()=>{console.log('SERVER STARTED');});
+app.listen(PORT,()=>{
+    console.log(`SERVER LISTENING AT ${PORT}`);
+});
