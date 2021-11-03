dc50cbd29d55cf912db5c3efaf4c105b305e3da2
hasin-shabbir <mhs581@nyu.edu>
2021-11-03 02:04:28 -0400

    communication between client & server established

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,16 @@ function App() {
   const [data,setData] = useState(null);
   
   useEffect(()=>{
+    console.log('hello');
     fetch('/api')
-      .then((res)=>res.json)
+      .then((res)=>res.json())
       .then((data)=>setData(data.message));
   },[]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           {!data ? "Loading..." : data}
         </p>
--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -23,58 +23,58 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/', (req, res) => {
-//     res.redirect('/login');
-// });
+app.get('/', (req, res) => {
+    res.redirect('/login');
+});
 
-// app.get('/register', (req, res) => {
-//     //registration page
-// });
+app.get('/register', (req, res) => {
+    //registration page
+});
 
-// app.post('/register', (req, res) => {
-//     //registration form handling
-//     res.redirect('/course-list'); //redirect to course-list upon successful registration by logging in the user
-// });
+app.post('/register', (req, res) => {
+    //registration form handling
+    res.redirect('/course-list'); //redirect to course-list upon successful registration by logging in the user
+});
 
-// app.get('/login', (req, res) => {
-//     //login page
-// });
+app.get('/login', (req, res) => {
+    //login page
+});
 
-// app.post('/login',(req,res)=>{
-//     //login form handling
-//     res.redirect('/course-list'); //redirect to course-list upon successful login
-// })
+app.post('/login',(req,res)=>{
+    //login form handling
+    res.redirect('/course-list'); //redirect to course-list upon successful login
+})
 
-// app.get('/course-list',(req,res)=>{
-//     //handle any query parameters here
-//     //displaying the course list page
-// });
+app.get('/course-list',(req,res)=>{
+    //handle any query parameters here
+    //displaying the course list page
+});
 
-// app.get('/review-page/:course',(req,res)=>{
-//     //rendering the review page for the particular course in the request parameters
-// });
+app.get('/review-page/:course',(req,res)=>{
+    //rendering the review page for the particular course in the request parameters
+});
 
-// app.get('/write-a-review/:course',(req,res)=>{
-//     //rendering the write a review page for the particular course in the request parameters
-// });
+app.get('/write-a-review/:course',(req,res)=>{
+    //rendering the write a review page for the particular course in the request parameters
+});
 
-// app.post('/write-a-review/:course',(req,res)=>{
-//     //handling form for writing a review for the particular course
-//     res.redirect('/write-a-review/:course') //redirect to reviews page for the course upon successful review post
-// });
+app.post('/write-a-review/:course',(req,res)=>{
+    //handling form for writing a review for the particular course
+    res.redirect('/write-a-review/:course') //redirect to reviews page for the course upon successful review post
+});
 
-// app.get('/edit-review/:course',(req,res)=>{
-//     //rendering the edit a review page for the logged in user for the particular course in the request parameters
-// });
+app.get('/edit-review/:course',(req,res)=>{
+    //rendering the edit a review page for the logged in user for the particular course in the request parameters
+});
 
-// app.post('/edit-review/:course',(req,res)=>{
-//     //handling form for editing a review for the particular course
-//     res.redirect('/edit-a-review/:course') //redirect to reviews page for the course upon successful review edit
-// });
+app.post('/edit-review/:course',(req,res)=>{
+    //handling form for editing a review for the particular course
+    res.redirect('/edit-a-review/:course') //redirect to reviews page for the course upon successful review edit
+});
 
 //ENDPOINT FOR API to respond to GET requests from the React App
 app.get("/api", (req, res) => {
-    res.json({ message: `Hello from server ${PORT}` });
+    res.json({ message: `Hello from hasin's server at ${PORT}` });
 });
 
 app.listen(PORT,()=>{
